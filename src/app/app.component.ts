e6351e6eef768125604f947ccce838da61b94c8f
Tom <tom@ohms.pro>
2022-04-11 13:19:38 +0200

    feat(list): change title and date to dynamic values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'addressbook';
+  appTitle = 'Adressbuch';
+  year = new Date().getFullYear();
 }
