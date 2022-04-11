bbcc73181a2125e9cd7a8d88efae5b2df660bd7b
Tom <tom@ohms.pro>
2022-04-11 15:42:29 +0200

    feat(app): add components for header, content, footer and address list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  appTitle = 'Adressbuch';
-  year = new Date().getFullYear();
+  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { ContentComponent } from './layout/content/content.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AddressListComponent } from './components/address-list/address-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    AddressListComponent
   ],
   imports: [
     BrowserModule
