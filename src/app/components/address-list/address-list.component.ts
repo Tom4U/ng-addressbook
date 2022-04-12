d8a2424b3128ed7aa8f05ebfa3ff14eec711d13f
Tom <tom@ohms.pro>
2022-04-12 10:21:43 +0200

    feat(list): add dynamic entries

--- src/app/components/address-list/address-list.component.ts
+++ src/app/components/address-list/address-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Address } from 'src/app/entities/address';
+import { Person } from 'src/app/entities/person';
 
 @Component({
   selector: 'app-address-list',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address-list.component.css']
 })
 export class AddressListComponent implements OnInit {
+  addresses: Address[] = [];
 
-  constructor() { }
+  constructor() {
+    this.addresses.push(this.createAddresses('+49123456789', 'Hans', 'Dampf'));
+    this.addresses.push(this.createAddresses('+49123456789', 'Fred', 'Feuerstein'));
+    this.addresses.push(this.createAddresses('+49123456789', 'Benjamin', 'Blümchen'));
+   }
 
   ngOnInit(): void {
+  }
+
+  private createAddresses(phone: string, firstname: string, lastname: string): Address {
+    const address = new Address();
+
+    address.phone = phone;
+    address.person = this.createPerson(firstname, lastname);
+
+    return address;
+  }
+
+  private createPerson(firstname: string, lastname: string): Person {
+    const person = new Person();
+    
+    person.firstname = firstname;
+    person.lastname = lastname;
+
+    return person;
   }
 
 }
---
+++ src/app/entities/address.ts
@@ -0,0 +1,6 @@
+import { Person } from "./person";
+
+export class Address {
+    person = new Person();
+    phone = '';
+}
