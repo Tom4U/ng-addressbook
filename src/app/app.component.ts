28602875578003abb13b6a5e50056bca721fc6ca
Tom <tom@ohms.pro>
2022-04-12 11:18:59 +0200

    feat(list) provide addresses from outside of component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Address } from './entities/address';
+import { Person } from './entities/person';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  addresses: Address[] = [];
   
-  
+  constructor() {
+    this.addresses.push(this.createAddresses('+49123456789', 'Hans', 'Dampf'));
+    this.addresses.push(this.createAddresses('+49123456789', 'Fred', 'Feuerstein'));
+    this.addresses.push(this.createAddresses('+49123456789', 'Benjamin', 'Blümchen'));
+   }
+
+   private createAddresses(phone: string, firstname: string, lastname: string): Address {
+    const address = new Address();
+
+    address.phone = phone;
+    address.person = this.createPerson(firstname, lastname);
+
+    return address;
+  }
+
+  private createPerson(firstname: string, lastname: string): Person {
+    const person = new Person();
+    
+    person.firstname = firstname;
+    person.lastname = lastname;
+
+    return person;
+  }
 }
--- src/app/components/address-list/address-list.component.ts
+++ src/app/components/address-list/address-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Address } from 'src/app/entities/address';
-import { Person } from 'src/app/entities/person';
 
 @Component({
   selector: 'app-address-list',
@@ -8,33 +7,10 @@ import { Person } from 'src/app/entities/person';
   styleUrls: ['./address-list.component.css']
 })
 export class AddressListComponent implements OnInit {
-  addresses: Address[] = [];
+  @Input() addresses: Address[] = [];
 
-  constructor() {
-    this.addresses.push(this.createAddresses('+49123456789', 'Hans', 'Dampf'));
-    this.addresses.push(this.createAddresses('+49123456789', 'Fred', 'Feuerstein'));
-    this.addresses.push(this.createAddresses('+49123456789', 'Benjamin', 'Blümchen'));
-   }
+  constructor() {}
 
   ngOnInit(): void {
   }
-
-  private createAddresses(phone: string, firstname: string, lastname: string): Address {
-    const address = new Address();
-
-    address.phone = phone;
-    address.person = this.createPerson(firstname, lastname);
-
-    return address;
-  }
-
-  private createPerson(firstname: string, lastname: string): Person {
-    const person = new Person();
-    
-    person.firstname = firstname;
-    person.lastname = lastname;
-
-    return person;
-  }
-
 }
